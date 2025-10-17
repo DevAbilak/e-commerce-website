5594ffe142248885d125922dfee54fbd508a9401
Yeabsira Zerihun <abilak0716@gmail.com>
2025-10-17 21:35:43 +0400

    start building orders, tracking and checkout pages.

--- src/pages/Tracking.jsx
+++ src/pages/Tracking.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
 import Navbar from '../components/Navbar'
+import '../styles/pages/tracking.css'
 
 const Tracking = () => {
   return (
     <div>
       <Navbar />
-      Tracking
+      <div className="tracking-page">
+        <div className="order-tracking">
+          <a className="back-to-orders-link link-primary" href="orders.html">
+            View all orders
+          </a>
+
+          <div className="delivery-date">
+            Arriving on Monday, June 13
+          </div>
+
+          <div className="product-info">
+            Black and Gray Athletic Cotton Socks - 6 Pairs
+          </div>
+
+          <div className="product-info">
+            Quantity: 1
+          </div>
+
+          <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+
+          <div className="progress-labels-container">
+            <div className="progress-label">
+              Preparing
+            </div>
+            <div className="progress-label current-status">
+              Shipped
+            </div>
+            <div className="progress-label">
+              Delivered
+            </div>
+          </div>
+
+          <div className="progress-bar-container">
+            <div className="progress-bar"></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
