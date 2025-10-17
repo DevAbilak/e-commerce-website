7591dba048f81cb1d2dd2c34d618da65c65ed7ed
Yeabsira Zerihun <abilak0716@gmail.com>
2025-10-17 20:55:50 +0400

    creating pages and setting up routes using react router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Home from './pages/Home'
+import Orders from './pages/Orders'
+import Checkout from './pages/Checkout'
+import Tracking from './pages/Tracking'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='orders' element={<Orders />} />
+        <Route path='checkout' element={<Checkout />} />
+        <Route path='tracking' element={<Tracking />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
